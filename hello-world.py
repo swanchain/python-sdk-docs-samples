2fd45ed724c85b5385ad7a5f7e8f043c6d551475
Charles Cao <caotaotao@gmail.com>
2024-08-25 17:10:45 -0400

    update hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,21 @@
+import os
+
+import dotenv
+import swan
+
+
+dotenv.load_dotenv()
+# default use testnet
+orchestrator = swan.resource(api_key=os.getenv("SWAN_API_KEY"), network='mainnet',service_name='Orchestrator')
+
+result = orchestrator.create_task(
+    app_repo_image='hello_world',
+    wallet_address=os.getenv("WALLET_ADDRESS"),
+    private_key=os.getenv("PRIVATE_KEY"),
+    hardware_id=0,  # 0 is the smallest hardware:CPU only · 2 vCPU · 2 GiB  Memory
+    # duration=172800,
+    # auto_pay=True
+)
+task_uuid = result['id']
+task_info = orchestrator.get_deployment_info(task_uuid=task_uuid)
+print(task_info)
