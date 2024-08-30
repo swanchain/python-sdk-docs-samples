0dd52ee089ed46befa9d715d4d131c26fe6d84b0
alphaflows <aaron.li@nbai.io>
2024-08-30 14:43:48 -0400

    update example

--- compute/hello-world.py
+++ compute/hello-world.py
@@ -1,5 +1,5 @@
 import os
-
+import time
 import dotenv
 import swan
 
@@ -9,13 +9,21 @@ def hello_world():
     orchestrator = swan.resource(api_key=os.getenv("SWAN_API_KEY"), network='mainnet',service_name='Orchestrator')
 
     result = orchestrator.create_task(
-        app_repo_image='hello_world',
+        repo_uri='https://github.com/swanchain/awesome-swanchain/tree/main/hello_world',
         wallet_address=os.getenv("WALLET_ADDRESS"),
         private_key=os.getenv("PRIVATE_KEY"),
+        instance_type='C1ae.small',
+        auto_pay=True
     )
     task_uuid = result['id']
     task_info = orchestrator.get_deployment_info(task_uuid=task_uuid)
     print(task_info)
+
+    # Wait for the task to be deployed
+    time.sleep(30)
+    # Show deployed application URLs
+    app_urls = orchestrator.get_real_url(task_uuid=task_uuid)
+    print(f"App URLs: {app_urls}")
 
 if __name__ == "__main__":
     # Load environment variables from the .env file
